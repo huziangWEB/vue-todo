c6cd182068561e8d1efc0ea9cc34e45dedf02d92
huziangWEB <704913915@qq.com>
2018-06-09 11:02:15 +0800

    2

---
+++ build/vue-loader.config.js
@@ -0,0 +1,6 @@
+module.exports = (isDev) => {
+	return {
+		preserveWhitepace: true, //防止vue文件模板空格被渲染
+		extractCSS: true
+	}
+}
---
+++ build/webpack.config.base.js
@@ -0,0 +1,53 @@
+/*
+* @Author: huziangWEB
+* @Date:   2018-06-09 01:05:05
+* @Last Modified by:   huziangWEB
+* @Last Modified time: 2018-06-09 02:05:27
+*/
+const path = require('path')
+
+
+
+const isDev = process.env.NODE_ENV === 'development'
+
+const config = {
+  target: 'web',
+  entry: path.join(__dirname, '../client/index.js'),
+  output: {
+    filename: 'bundle.[hash:8].js',
+    path: path.join(__dirname, '../dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(gif|jpg|jpeg|png|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1024,
+              name: 'resources/[path][name]-[hash:8].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
+
+
+
+module.exports = config
